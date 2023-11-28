1c5cb062687d6555ea6c268a1dd78717a5268380
ChrisMet <81528844+C-Metcalf@users.noreply.github.com>
2023-11-28 09:49:56 -0500

    Create BashPIDTester.sh

---
+++ BashPIDTester.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+sudo apt install wtype
+sudo apt-get install can-utils
+
+mkdir code 
+cd code
+python -m venv venv 
+cd venv/bin
+source activate
+cd ../..
+
+pip install PySide6
+pip install pyserial
+pip install pglive
+
+git clone https://github.com/C-Metcalf/5C-MC-GUI.git
